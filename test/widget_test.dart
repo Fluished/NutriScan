c8f74b12ea79c293233ed1b9b81dd876ef5055dd
Fluished <46325802+Fluished@users.noreply.github.com>
2025-04-01 03:22:03 +0800

    initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:NutriScan/main.dart';
+import 'package:nutriscan/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
